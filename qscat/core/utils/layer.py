002afe84dafd89da91610a588e7bcf16fcc8838d
Louis Facun <louisfacun@gmail.com>
2024-04-16 01:07:49 +0800

    test: add test for `is_field_in_layer` function

--- qscat/core/utils/layer.py
+++ qscat/core/utils/layer.py
@@ -1,4 +1,13 @@
 def is_field_in_layer(field, layer):
+    """Check if field exists in the layer.
+    
+    Args:
+        field (str): The field name to check.
+        layer (QgsVectorLayer): The layer to check the field in.
+    
+    Returns:
+        bool: True if field exists, False otherwise.
+    """
     if layer.fields().indexFromName(field) == -1:
         return False
     return True
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,9 +3,15 @@
 
 import pytest
 
+from qgis.core import QgsVectorLayer
+from qgis.testing import start_app
+
 from qscat.core.utils.date import convert_to_decimal_year
 from qscat.core.utils.date import extract_month_year
 from qscat.core.utils.date import get_day_of_year
+from qscat.core.utils.layer import is_field_in_layer
+
+start_app()
 
 
 def test_utils_date():
@@ -37,3 +43,17 @@ def test_utils_date():
     assert convert_to_decimal_year("2/2022") == 2022.09
     assert convert_to_decimal_year("3/2023") == 2023.16
     assert convert_to_decimal_year("3/2024") == 2024.17
+
+
+def test_utils_is_field_in_layer():
+    """Test if field is in the layer function."""
+    
+    layer = QgsVectorLayer(
+        'LineString?crs=EPSG:4326&field=id:integer&field=name:string(20)', 
+        'test_layer', 
+        'memory'
+    )
+
+    assert is_field_in_layer('id', layer) == True
+    assert is_field_in_layer('name', layer) == True
+    assert is_field_in_layer('non_existent', layer) == False
