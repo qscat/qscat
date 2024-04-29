9a828c0e03c012421d96774924d5d15c1b81470b
Louis Facun <louisfacun@gmail.com>
2024-04-29 23:00:58 +0800

    style(docs): format with `black`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,38 +2,42 @@
 
 # -- Project information
 
-project = 'QSCAT'
-copyright = '2024, QSCAT'
+project = "QSCAT"
+copyright = "2024, QSCAT"
 author = """Louis Facun, Fernando Siringan, Floribeth Cuison, Ara Rivina Malaya,
 Ma. Yvainne Sta. Maria, Jamela Jirah Clemente, Angelo Maon, Ellen Mae Carmelo, 
 and Rodel Ducao
 """
 
-release = '0.3.1'
-version = '0.3.1'
+release = "0.3.1"
+version = "0.3.1"
 
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinxcontrib.bibtex',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.bibtex",
+    "sphinx_sitemap",
 ]
 
+html_baseurl = "https://qscat.github.io/docs/"
+sitemap_url_scheme = "{link}"
+
 # -- Bibtex configuration
-bibtex_bibfiles = ['refs.bib']
-bibtex_reference_style = 'author_year'
+bibtex_bibfiles = ["refs.bib"]
+bibtex_reference_style = "author_year"
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -----------------------------------------------------------------------------
 # HTML output
@@ -41,15 +45,23 @@ templates_path = ['_templates']
 
 html_theme = "pydata_sphinx_theme"
 
+html_context = {
+    "default_mode": "light",
+    "github_user": "qscat",
+    "github_repo": "qscat",
+    "github_version": "master",
+    "doc_path": "docs/source",
+}
+
 html_theme_options = {
-    "show_nav_level": 2,
+    "show_nav_level": 1,
     "logo": {
-      "image_light": "_static/qscat-logo.svg",
-      "image_dark": "_static/qscat-logo-dark.svg",
-   },
-   "external_links": [
-      {"name": "GitHub", "url": "https://github.com/qscat/qscat"},
-    ],
+        "image_light": "_static/qscat-logo.svg",
+        "image_dark": "_static/qscat-logo-dark.svg",
+    },
+    # "external_links": [
+    #     {"name": "GitHub", "url": "https://github.com/qscat/qscat"},
+    # ],
     "icon_links": [
         {
             "name": "Facebook",
@@ -60,20 +72,31 @@ html_theme_options = {
             "name": "Twitter",
             "url": "https://twitter.com/QSCATplugin",
             "icon": "fa-brands fa-square-twitter",
-        }
+        },
     ],
+    "secondary_sidebar_items": ["page-toc", "edit-this-page", "sourcelink"],
+    "footer_start": ["copyright", "sphinx-version"],
+    "footer_end": [],
+    "use_edit_page_button": True,
+    "github_url": "https://github.com/qscat/qscat",
+    "show_version_warning_banner": True,
+    # "primary_sidebar_end": ["indices.html", "sidebar-ethical-ads.html"],
+    # #"content_footer_items": ["last-updated"],
+    # "switcher": {
+    #     "json_url": "switcher.json",
+    # }
 }
 
+
 html_title = "%s v%s Documentation" % (project, version)
-html_static_path = ['_static']
-html_sidebars = {'**': ['search-field', 'sidebar-nav-bs', 'sidebar-ethical-ads']}
+html_static_path = ["_static"]
+html_sidebars = {"**": ["search-field", "sidebar-nav-bs", "sidebar-ethical-ads"]}
 html_favicon = "favicon.ico"
-html_last_updated_fmt = '%Y %b %d, %H:%M %z'
-html_css_files = ['css/qscat.css']
-html_context = {"default_mode": "light"}
+html_last_updated_fmt = "%Y %b %d, %H:%M %z"
+html_css_files = ["css/qscat.css"]
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
 
 # Enable numeric figure references
 numfig = True
