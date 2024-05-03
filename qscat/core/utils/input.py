a49a3f6d448b4ab9499e4ad00463c390257c880d
Louis Facun <louisfacun@gmail.com>
2024-05-03 16:58:22 +0800

    style: ruff formatter

--- qscat/core/utils/input.py
+++ qscat/core/utils/input.py
@@ -11,18 +11,18 @@ from qscat.core.constants import Statistic
 
 def get_project_settings_input_params(qscat):
     """Returns a dictionary containing the input parameters in Project Settings Tab.
-    
+
     Args:
         qscat (QscatPlugin): QscatPlugin instance.
-        
+
     Returns:
         dict: A dictionary containing the input parameters in Project Settings Tab.
     """
     return {
-        'crs_id':             QgsProject.instance().crs().authid(),
-        'author_full_name':   qscat.dockwidget.le_proj_author_full_name.text(),
-        'author_affiliation': qscat.dockwidget.le_proj_author_affiliation.text(),
-        'author_email':       qscat.dockwidget.le_proj_author_email.text(),
+        "crs_id": QgsProject.instance().crs().authid(),
+        "author_full_name": qscat.dockwidget.le_proj_author_full_name.text(),
+        "author_affiliation": qscat.dockwidget.le_proj_author_affiliation.text(),
+        "author_email": qscat.dockwidget.le_proj_author_email.text(),
     }
 
 
@@ -36,14 +36,14 @@ def get_baseline_input_params(qscat):
         dict: A dictionary containing the input parameters in Baseline Tab.
     """
     return {
-        'baseline_layer':                     qscat.dockwidget.qmlcb_baseline_baseline_layer.currentLayer(),
-        'is_baseline_placement_sea':          qscat.dockwidget.rb_baseline_placement_sea.isChecked(),
-        'is_baseline_placement_land':         qscat.dockwidget.rb_baseline_placement_land.isChecked(),
-        'is_baseline_orientation_land_right': qscat.dockwidget.rb_baseline_orientation_land_right.isChecked(),
-        'is_baseline_orientation_land_left':  qscat.dockwidget.rb_baseline_orientation_land_left.isChecked(),
-        'placement_field':                    qscat.dockwidget.qfcb_baseline_placement_field.currentField(),
-        'orientation_field':                  qscat.dockwidget.qfcb_baseline_orientation_field.currentField(),
-        'transect_length_field':              qscat.dockwidget.qfcb_baseline_length_field.currentField(),
+        "baseline_layer": qscat.dockwidget.qmlcb_baseline_baseline_layer.currentLayer(),
+        "is_baseline_placement_sea": qscat.dockwidget.rb_baseline_placement_sea.isChecked(),
+        "is_baseline_placement_land": qscat.dockwidget.rb_baseline_placement_land.isChecked(),
+        "is_baseline_orientation_land_right": qscat.dockwidget.rb_baseline_orientation_land_right.isChecked(),
+        "is_baseline_orientation_land_left": qscat.dockwidget.rb_baseline_orientation_land_left.isChecked(),
+        "placement_field": qscat.dockwidget.qfcb_baseline_placement_field.currentField(),
+        "orientation_field": qscat.dockwidget.qfcb_baseline_orientation_field.currentField(),
+        "transect_length_field": qscat.dockwidget.qfcb_baseline_length_field.currentField(),
     }
 
 
@@ -57,16 +57,16 @@ def get_shorelines_input_params(qscat):
         dict: A dictionary containing the input parameters in Shorelines Tab.
     """
     return {
-        'shorelines_layer':         qscat.dockwidget.qmlcb_shorelines_shorelines_layer.currentLayer(),
-        'default_data_uncertainty': qscat.dockwidget.le_shorelines_default_data_unc.text(),
-        'date_field':               qscat.dockwidget.qfcb_shorelines_date_field.currentField(),
-        'uncertainty_field':        qscat.dockwidget.qfcb_shorelines_uncertainty_field.currentField(),
+        "shorelines_layer": qscat.dockwidget.qmlcb_shorelines_shorelines_layer.currentLayer(),
+        "default_data_uncertainty": qscat.dockwidget.le_shorelines_default_data_unc.text(),
+        "date_field": qscat.dockwidget.qfcb_shorelines_date_field.currentField(),
+        "uncertainty_field": qscat.dockwidget.qfcb_shorelines_uncertainty_field.currentField(),
     }
 
 
 def get_transects_input_params(qscat):
     """Returns a dictionary containing the input parameters in Transects Tab.
-    
+
     Args:
         qscat (QscatPlugin): QscatPlugin instance.
 
@@ -74,13 +74,13 @@ def get_transects_input_params(qscat):
         dict: A dictionary containing the input parameters in Transects Tab.
     """
     return {
-        'layer_output_name':         qscat.dockwidget.le_transects_layer_output_name.text(),
-        'is_by_transect_spacing':    qscat.dockwidget.rb_transects_by_transect_spacing.isChecked(),
-        'is_by_number_of_transects': qscat.dockwidget.rb_transects_by_number_of_transects.isChecked(),
-        'by_transect_spacing':       qscat.dockwidget.qsb_transects_by_transect_spacing.text(),
-        'by_number_of_transects':    qscat.dockwidget.qsb_transects_by_number_of_transects.text(),
-        'length':                    qscat.dockwidget.qsb_transects_length.text(),
-        'smoothing_distance':        qscat.dockwidget.qsb_transects_smoothing_distance.text(),
+        "layer_output_name": qscat.dockwidget.le_transects_layer_output_name.text(),
+        "is_by_transect_spacing": qscat.dockwidget.rb_transects_by_transect_spacing.isChecked(),
+        "is_by_number_of_transects": qscat.dockwidget.rb_transects_by_number_of_transects.isChecked(),
+        "by_transect_spacing": qscat.dockwidget.qsb_transects_by_transect_spacing.text(),
+        "by_number_of_transects": qscat.dockwidget.qsb_transects_by_number_of_transects.text(),
+        "length": qscat.dockwidget.qsb_transects_length.text(),
+        "smoothing_distance": qscat.dockwidget.qsb_transects_smoothing_distance.text(),
     }
 
 
@@ -94,24 +94,30 @@ def get_shoreline_change_input_params(qscat):
         dict: A dictionary containing the input parameters in Shoreline Change Tab.
     """
     return {
-        'transects_layer':                 qscat.dockwidget.qmlcb_shoreline_change_transects_layer.currentLayer(),
-        'transects_layer_widget':          qscat.dockwidget.qmlcb_shoreline_change_transects_layer,
-        'is_clip_transects':               qscat.dockwidget.cb_stats_clip_transects.isChecked(),
-        'is_choose_by_distance':           qscat.dockwidget.rb_choose_by_distance.isChecked(),
-        'is_choose_by_distance_farthest':  qscat.dockwidget.rb_choose_by_distance_farthest.isChecked(),
-        'is_choose_by_distance_closest':   qscat.dockwidget.rb_choose_by_distance_closest.isChecked(),
-        'is_choose_by_placement':          qscat.dockwidget.rb_choose_by_placement.isChecked(),
-        'is_choose_by_placement_seaward':  qscat.dockwidget.rb_choose_by_placement_seaward.isChecked(),
-        'is_choose_by_placement_landward': qscat.dockwidget.rb_choose_by_placement_landward.isChecked(),
-        'selected_stats':                  get_shoreline_change_stat_selected(qscat),
-        'oldest_date':                     qscat.dockwidget.cb_shoreline_change_oldest_date.currentText(),
-        'newest_date':                     qscat.dockwidget.cb_shoreline_change_newest_date.currentText(),
-        'oldest_year':                     convert_to_decimal_year(qscat.dockwidget.cb_shoreline_change_oldest_date.currentText()),
-        'newest_year':                     convert_to_decimal_year(qscat.dockwidget.cb_shoreline_change_newest_date.currentText()),
-        'confidence_interval':             float(qscat.dockwidget.qdsb_stats_confidence_interval.text()),
-        'epr_unc':                         get_epr_unc_from_input(qscat),
-        'highest_unc':                     get_highest_unc_from_input(qscat),
-        'years_uncs':                      get_shorelines_years_uncs_from_input(qscat),
+        "transects_layer": qscat.dockwidget.qmlcb_shoreline_change_transects_layer.currentLayer(),
+        "transects_layer_widget": qscat.dockwidget.qmlcb_shoreline_change_transects_layer,
+        "is_clip_transects": qscat.dockwidget.cb_stats_clip_transects.isChecked(),
+        "is_choose_by_distance": qscat.dockwidget.rb_choose_by_distance.isChecked(),
+        "is_choose_by_distance_farthest": qscat.dockwidget.rb_choose_by_distance_farthest.isChecked(),
+        "is_choose_by_distance_closest": qscat.dockwidget.rb_choose_by_distance_closest.isChecked(),
+        "is_choose_by_placement": qscat.dockwidget.rb_choose_by_placement.isChecked(),
+        "is_choose_by_placement_seaward": qscat.dockwidget.rb_choose_by_placement_seaward.isChecked(),
+        "is_choose_by_placement_landward": qscat.dockwidget.rb_choose_by_placement_landward.isChecked(),
+        "selected_stats": get_shoreline_change_stat_selected(qscat),
+        "oldest_date": qscat.dockwidget.cb_shoreline_change_oldest_date.currentText(),
+        "newest_date": qscat.dockwidget.cb_shoreline_change_newest_date.currentText(),
+        "oldest_year": convert_to_decimal_year(
+            qscat.dockwidget.cb_shoreline_change_oldest_date.currentText()
+        ),
+        "newest_year": convert_to_decimal_year(
+            qscat.dockwidget.cb_shoreline_change_newest_date.currentText()
+        ),
+        "confidence_interval": float(
+            qscat.dockwidget.qdsb_stats_confidence_interval.text()
+        ),
+        "epr_unc": get_epr_unc_from_input(qscat),
+        "highest_unc": get_highest_unc_from_input(qscat),
+        "years_uncs": get_shorelines_years_uncs_from_input(qscat),
     }
 
 
@@ -148,8 +154,8 @@ def get_area_change_input_params(qscat):
         dict: A dictionary containing the input parameters in Area Change Tab.
     """
     return {
-        'polygon_layer': qscat.dockwidget.qmlcb_area_change_polygon_layer.currentLayer(),
-        'stat_layer':    qscat.dockwidget.qmlcb_area_change_stat_layer.currentLayer(),
+        "polygon_layer": qscat.dockwidget.qmlcb_area_change_polygon_layer.currentLayer(),
+        "stat_layer": qscat.dockwidget.qmlcb_area_change_stat_layer.currentLayer(),
     }
 
 
@@ -163,10 +169,10 @@ def get_summary_report_input_params(qscat):
         dict: A dictionary containing the input parameters in Summary Report Tab.
     """
     return {
-        'is_report':                  qscat.dockwidget.cb_enable_report_generation.isChecked(),
-        'is_shoreline_change_report': qscat.dockwidget.cb_enable_shoreline_change_report.isChecked(),
-        'is_area_change_report':      qscat.dockwidget.cb_enable_area_change_report.isChecked(),
-        'is_forecasting_report':      qscat.dockwidget.cb_enable_forecasting_report.isChecked(),
+        "is_report": qscat.dockwidget.cb_enable_report_generation.isChecked(),
+        "is_shoreline_change_report": qscat.dockwidget.cb_enable_shoreline_change_report.isChecked(),
+        "is_area_change_report": qscat.dockwidget.cb_enable_area_change_report.isChecked(),
+        "is_forecasting_report": qscat.dockwidget.cb_enable_forecasting_report.isChecked(),
     }
 
 
@@ -176,7 +182,7 @@ def get_shorelines_dates(qscat):
 
     Args:
         qscat (QscatPlugin): QscatPlugin instance.
-    
+
     Returns:
         list[str]: A list of shoreline dates in the format 'mm/yyyy'.
     """
@@ -242,7 +248,7 @@ def is_no_unc_value(unc_field_value):
         return True
     else:
         return False
-    
+
 
 def get_epr_unc_from_input(self):
     default_unc = float(self.dockwidget.le_shorelines_default_data_unc.text())
@@ -257,8 +263,10 @@ def get_epr_unc_from_input(self):
     if is_no_unc_value(oldest_date_unc):
         oldest_date_unc = default_unc
 
-    EPR_unc_numerator = (newest_date_unc**2 + oldest_date_unc**2)
-    EPR_unc = math.sqrt(EPR_unc_numerator) \
-        / (convert_to_decimal_year(newest_year_txt)-convert_to_decimal_year(oldest_year_txt))
+    EPR_unc_numerator = newest_date_unc**2 + oldest_date_unc**2
+    EPR_unc = math.sqrt(EPR_unc_numerator) / (
+        convert_to_decimal_year(newest_year_txt)
+        - convert_to_decimal_year(oldest_year_txt)
+    )
 
     return round(EPR_unc, 2)
